73a46d59d8681496f363583d97036465a69fbf3d
kyujin <spqj951@gmail.com>
2022-07-22 03:26:16 +0900

    websocket

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
@@ -15,4 +15,24 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Annonymous";
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected from the browser"));
+  socket.on("message", (message) => {
+    const parsed = JSON.parse(message);
+    switch (parsed.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname} : ${parsed.payload}`)
+        );
+        console.log(socket.nickname);
+      case "nickname":
+        socket["nickname"] = parsed.payload;
+    }
+  });
+});
 server.listen(3000, handleListen);
